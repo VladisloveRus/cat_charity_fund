f57e6a7ac4af6f32d1036fa0451f664ccf88ab95
Vladislav Cherepanov <V.V.Cherepanov@bk.ru>
2023-09-04 21:22:34 +0500

    third fix for tests

--- app/core/config.py
+++ app/core/config.py
@@ -4,8 +4,8 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'SECRET'
-    app_title: str
-    app_description: str
+    app_title: str = 'Кошачий благотворительный фонд'
+    app_description: str = 'Сервис для поддержки котиков!'
 
     class Config:
         env_file = '.env'
